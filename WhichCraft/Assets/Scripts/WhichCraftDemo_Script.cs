8ea5b3186251d1738daadf0c97d2dfc15eb481a9
amoreschristian <amoresc@my.yorku.ca>
2023-02-13 22:44:36 -0500

    WhichCraftDemo- Finished
    Finished version of the WhichCraftDemo

--- WhichCraft/Assets/Scripts/WhichCraftDemo_Script.cs
+++ WhichCraft/Assets/Scripts/WhichCraftDemo_Script.cs
@@ -14,29 +14,54 @@ public class WhichCraftDemo_Script : MonoBehaviour
         }
     }
 
-    public void PlayGame()
+    public void ExitGame()
     {
-        SceneManager.LoadScene("EndlessRunner"); //this will have the name of your main game scene
+        Application.Quit();
     }
-    public void ClickBlue()
+
+    public void ClickBlue1()
     {
         SceneManager.LoadScene("Blue1");
     }
 
-    public void ClickYellow()
+    public void ClickYellow1()
     {
-       
+        SceneManager.LoadScene("Yellow1");
     }
 
-    public void ClickRed()
+    public void ClickRed1()
+    {
+        SceneManager.LoadScene("Red1");
+    }
+
+    public void Blue1Red2()
     {
         SceneManager.LoadScene("Blue1Red2");
     }
 
-
-    public void ExitGame()
+    public void Blue1Yellow2()
     {
-        Application.Quit();
+        SceneManager.LoadScene("Blue1Yellow2");
+    }
+
+    public void Red1Blue2()
+    {
+        SceneManager.LoadScene("Red1Blue2");
+    }
+
+    public void Red1Yellow2()
+    {
+        SceneManager.LoadScene("Red1Yellow2");
+    }
+
+    public void Yellow1Blue2()
+    {
+        SceneManager.LoadScene("Yellow1Blue2");
+    }
+
+    public void Yellow1Red2()
+    {
+        SceneManager.LoadScene("Yellow1Red2");
     }
 
     public void ClickedReset()
@@ -44,8 +69,18 @@ public class WhichCraftDemo_Script : MonoBehaviour
         SceneManager.LoadScene("WhichCraftDemo");
     }
 
-    public void ClickedCombine()
+    public void ClickedCombine_Purple()
     {
-        SceneManager.LoadScene("Blue1Red2_Combine");
+        SceneManager.LoadScene("CreatedPurple");
+    }
+
+    public void ClickedCombine_Green()
+    {
+        SceneManager.LoadScene("CreatedGreen");
+    }
+
+    public void ClickedCombine_Orange()
+    {
+        SceneManager.LoadScene("CreatedOrange");
     }
 }
