3b23b81ad3fc3027f551c7fdbbfc221e68a30f45
amoreschristian <amoresc@my.yorku.ca>
2023-03-03 16:53:38 -0500

    MiniGame1 - Clicking game
    yaa boi keep clicking

---
+++ WhichCraft/Assets/Scripts/EnchantController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using TMPro;
+using UnityEngine.UI;
+
+public class EnchantController : MonoBehaviour
+{
+    public Image bar;
+    public float fillAmount = 0.02f;
+
+    public float timeLeft;
+    public bool timerOn = false;
+
+    public TMP_Text timeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar.fillAmount = 0f;
+        timerOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerOn)
+        {
+            if(timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                updateTimer(timeLeft);
+
+                if(bar.fillAmount == 1)
+                {
+                    SceneManager.LoadScene("MiniGame1_enchant_WinMenu"); 
+                }
+            }
+            else
+            {
+                Debug.Log("Time is Up!");
+                timerOn = false;
+                timeLeft = 0;
+                SceneManager.LoadScene("MiniGame1_enchant_LoseMenu");
+
+            }
+        }
+    }
+
+    public void EnchantButtonClick ()
+    {
+        bar.fillAmount += fillAmount;
+    }
+
+    public void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timeText.text = timeLeft.ToString("0.#");
+    }
+}
