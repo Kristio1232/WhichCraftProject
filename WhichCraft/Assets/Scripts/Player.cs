655c9523483b8f9c7e7c5dc5fe9e63b996f0c623
Stefan <sciausu@outlook.com>
2023-03-07 17:39:57 -0500

    potion store updates
    - added collisions
    - added triggers for potion and ingredients

--- WhichCraft/Assets/Scripts/Player.cs
+++ WhichCraft/Assets/Scripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
     public int movementSpeed = 5;
     public Rigidbody2D rb;
     private Vector2 moveDirection;
+    public GameObject bottleSelect;
+    public GameObject ingredientSelect;
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +24,54 @@ public class Player : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
         moveDirection = new Vector2(moveX, moveY).normalized;
 
+        
        
     }
+    
     void FixedUpdate()
     {
         rb.velocity = new Vector2(moveDirection.x * movementSpeed, moveDirection.y * movementSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.tag == "bottle") 
+        { 
+            Debug.Log("Bottle"); 
+        }
+
+        if (collision.tag == "ingredient")
+        {
+            Debug.Log("ingredient");
+        }
+
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "bottle")
+        {
+            Debug.Log("BottleStay");
+        }
+        if (collision.tag == "ingredient")
+        {
+            Debug.Log("ingredientStay");
+        }
+
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "bottle")
+        {
+            Debug.Log("BottleLeave");
+        }
+        if (collision.tag == "ingredient")
+        {
+            Debug.Log("ingredientLeave");
+        }
+
+
     }
 }
