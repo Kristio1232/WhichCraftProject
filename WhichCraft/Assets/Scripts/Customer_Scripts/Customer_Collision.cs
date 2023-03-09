86ba1884b53f4891bbf2911cea0c346fff70b608
amoreschristian <amoresc@my.yorku.ca>
2023-03-09 15:12:50 -0500

    changes to customer animation and collision

--- WhichCraft/Assets/Scripts/Customer_Scripts/Customer_Collision.cs
+++ WhichCraft/Assets/Scripts/Customer_Scripts/Customer_Collision.cs
@@ -8,16 +8,45 @@ public class Customer_Collision : MonoBehaviour
     public Animator anim;
     public GameObject player;
     public GameObject customer;
+    public SpriteRenderer sprite;
+    public bool customerOnCounter;
+
+    [SerializeField]
+    private Transform[] waypoints;
+
+    [SerializeField]
+    private float moveSpeed = 2f;
+    private int waypointIndex = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        customerOnCounter = false;
+        sprite = GetComponent<SpriteRenderer>();
         Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), customer.GetComponent<Collider2D>(), true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (customerOnCounter)
+        {
+            MoveToExit();
+        }
+    }
+
+    private void MoveToExit()
+    {
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+
+            // Move Customer from current waypoint to the next one
+            // using MoveTowards method
+            transform.position = Vector2.MoveTowards(transform.position,
+               waypoints[waypointIndex].transform.position,
+               moveSpeed * Time.deltaTime);
+        }
     }
 
 
@@ -37,16 +66,20 @@ public class Customer_Collision : MonoBehaviour
         {
             Debug.Log("Customer Stops Here");
             anim.SetBool("Idle", true);
-        }
-        else
-        {
-            anim.SetBool("Idle", false);
+            customerOnCounter = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        anim.SetBool("Idle", false);
+        anim.SetBool("WalkOut", true);
+        sprite.flipX = false;
+    }
 
-
+    public void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+        float seconds = Mathf.FloorToInt(currentTime % 60);
     }
 }
