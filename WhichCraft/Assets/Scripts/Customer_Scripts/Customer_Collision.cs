dc6a6165148a2242b8f756fafbd008edf4b4b9e5
amoreschristian <amoresc@my.yorku.ca>
2023-03-09 16:05:08 -0500

    Fixed customer's animation when entering/leaving

--- WhichCraft/Assets/Scripts/Customer_Scripts/Customer_Collision.cs
+++ WhichCraft/Assets/Scripts/Customer_Scripts/Customer_Collision.cs
@@ -8,8 +8,9 @@ public class Customer_Collision : MonoBehaviour
     public Animator anim;
     public GameObject player;
     public GameObject customer;
+    public GameObject customerStopsHere;
     public SpriteRenderer sprite;
-    public bool customerOnCounter;
+    public bool customerLeaving;
 
     [SerializeField]
     private Transform[] waypoints;
@@ -18,11 +19,12 @@ public class Customer_Collision : MonoBehaviour
     private float moveSpeed = 2f;
     private int waypointIndex = 0;
 
+    public bool timerOn;
+    public float timeLeft = 15f;
 
     // Start is called before the first frame update
     void Start()
     {
-        customerOnCounter = false;
         sprite = GetComponent<SpriteRenderer>();
         Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), customer.GetComponent<Collider2D>(), true);
     }
@@ -30,25 +32,32 @@ public class Customer_Collision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (customerOnCounter)
+        if (timerOn)
         {
-            MoveToExit();
+            if (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                updateTimer(timeLeft);
+            }
+            else
+            {
+                timeLeft = 0;
+                customerStopsHere.SetActive(false);
+                MoveToExit();
+            }
         }
+
     }
 
     private void MoveToExit()
     {
         if (waypointIndex <= waypoints.Length - 1)
         {
-
-            // Move Customer from current waypoint to the next one
-            // using MoveTowards method
             transform.position = Vector2.MoveTowards(transform.position,
                waypoints[waypointIndex].transform.position,
                moveSpeed * Time.deltaTime);
         }
     }
-
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -56,22 +65,25 @@ public class Customer_Collision : MonoBehaviour
         {
             Debug.Log("Customer Gone!");
             Destroy(gameObject);
+            customerStopsHere.SetActive(true);
+        }
+
+        if (other.CompareTag("CustomerStops"))
+        {
+            Debug.Log("Customer Stops Here");
+            anim.SetBool("Idle", true);
         }
 
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.CompareTag("CustomerStops"))
-        {
-            Debug.Log("Customer Stops Here");
-            anim.SetBool("Idle", true);
-            customerOnCounter = true;
-        }
+
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        Debug.Log("Customer is Leaving");
         anim.SetBool("Idle", false);
         anim.SetBool("WalkOut", true);
         sprite.flipX = false;
