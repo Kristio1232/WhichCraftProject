25c3911f1306c8a91b1f0a1a4b3b035d3f8447eb
laibaz16 <laibazahid16@gmail.com>
2023-03-09 21:37:25 -0500

    test commit
    test commit

--- WhichCraft/Assets/Scripts/Potion.cs
+++ WhichCraft/Assets/Scripts/Potion.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class In : MonoBehaviour
+public class Potion: Player
 {
     // Start is called before the first frame update
     void Start()
     {
+        public value = 0;
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
