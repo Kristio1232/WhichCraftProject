a86abe87516689d0e4317c30756ce7d3382bb11d
Christopher Budd <76147782+Kristio1232@users.noreply.github.com>
2023-03-28 13:32:42 -0400

    Random Money

--- WhichCraft/Assets/Scripts/Customer.cs
+++ WhichCraft/Assets/Scripts/Customer.cs
@@ -15,13 +15,18 @@ public class Customer : MonoBehaviour
     //Selling Potion Info
     private int money;
     private string potionCode;
-    private double satisfaction;
+    //private double satisfaction;
     
     // Start is called before the first frame update
     // Random money and potionCode
     void Start()
     {
-        satisfaction = 1.00;
+        //satisfaction = 1.00;
+        
+        money  = Random.Range(15, 101);
+        if (Random.Range(1, 10000) == 1){
+            money  = Random.Range(1000, 10000);
+        }
     }
 
     // Update is called once per frame
