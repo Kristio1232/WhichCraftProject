fadac96ce97d38f37b4550dd98b78b590feffd17
amoreschristian <amoresc@my.yorku.ca>
2023-04-01 15:35:59 -0400

    Revert "potion sprite change updated"
    This reverts commit f4dcbc334136673fd1e2b177b97b47533fb58e1c.

--- WhichCraft/Assets/Scripts/PotionSpriteChange.cs
+++ WhichCraft/Assets/Scripts/PotionSpriteChange.cs
@@ -22,14 +22,15 @@ public class PotionSpriteChange : MonoBehaviour
     void Start()
     {
         potionCode = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        pc = potionCode.getSelectedItems();
         potionShown = this.GetComponent<Image>();
+        Debug.Log(pc);
     }
 
     // Update is called once per frame
     void Update()
     {
-        pc = potionCode.getSelectedItems();
-        if (pc == "154" || pc == "145" || pc == "415" || pc == "451" || pc == "514" || pc == "541")
+        if(pc == "154" || pc == "145" || pc == "415" || pc == "451" || pc == "514" || pc == "541")
         {
             potionShown.sprite = sprite_potion145;
         }
@@ -53,5 +54,6 @@ public class PotionSpriteChange : MonoBehaviour
         {
             potionShown.sprite = sprite_potion246;
         }
+
     }
 }
