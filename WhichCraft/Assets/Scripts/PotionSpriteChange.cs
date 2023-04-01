f4559ab353404096153243f6224779ae07f99e57
amoreschristian <amoresc@my.yorku.ca>
2023-03-31 23:13:13 -0400

    fixed potionSpriteChange script

--- WhichCraft/Assets/Scripts/PotionSpriteChange.cs
+++ WhichCraft/Assets/Scripts/PotionSpriteChange.cs
@@ -21,23 +21,39 @@ public class PotionSpriteChange : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        potionShown = this.GetComponent<Image>();
+        potionCode = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         pc = potionCode.getSelectedItems();
+        potionShown = this.GetComponent<Image>();
         Debug.Log(pc);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(pc == "154" || pc == "145")
+        if(pc == "154" || pc == "145" || pc == "415" || pc == "451" || pc == "514" || pc == "541")
         {
             potionShown.sprite = sprite_potion145;
         }
+        else if(pc == "256" || pc == "265" || pc == "526" || pc == "562" || pc == "625" || pc == "652")
+        {
+            potionShown.sprite = sprite_potion256;
+        }
+        else if (pc == "346" || pc == "364" || pc == "436" || pc == "463" || pc == "634" || pc == "643")
+        {
+            potionShown.sprite = sprite_potion346;
+        }
+        else if (pc == "156" || pc == "165" || pc == "516" || pc == "561" || pc == "615" || pc == "651")
+        {
+            potionShown.sprite = sprite_potion156;
+        }
+        else if (pc == "345" || pc == "354" || pc == "435" || pc == "453" || pc == "534" || pc == "543")
+        {
+            potionShown.sprite = sprite_potion345;
+        }
+        else if (pc == "246" || pc == "264" || pc == "426" || pc == "462" || pc == "624" || pc == "642")
+        {
+            potionShown.sprite = sprite_potion246;
+        }
+
     }
-
-    void changeImage()
-    {
-
-    }
-
 }
